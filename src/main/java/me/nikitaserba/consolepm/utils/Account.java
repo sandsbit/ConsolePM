f7c06f22e12d7d96e5c030b4228582342fdbf6aa
Nikita Serba <nikitaserba@icloud.com>
2025-04-01 19:25:23 +0300

    Fix account class structure

--- src/main/java/me/nikitaserba/consolepm/utils/Account.java
+++ src/main/java/me/nikitaserba/consolepm/utils/Account.java
@@ -1,21 +1,26 @@
 package me.nikitaserba.consolepm.utils;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.Date;
 
 public class Account {
 
-    private final String name;
-    private final String username;
-    private final String email;
-    private byte[] passwordEncrypted;
-    private final Date created;
+    private String name;
+    private String username;
+    private String email;
+    private String passwordEncrypted;
+    private Date created;
 
     public Account(String name, String username, String email, byte[] passwordEncrypted, Date created) {
         this.name = name;
         this.username = username;
         this.email = email;
-        this.passwordEncrypted = passwordEncrypted;
+        this.passwordEncrypted = passwordEncrypted == null ? null : new String(passwordEncrypted, StandardCharsets.UTF_8);
         this.created = created;
+    }
+
+    public Account() {
     }
 
     public String getName() {
@@ -31,7 +36,7 @@ public class Account {
     }
 
     public byte[] getPasswordEncrypted() {
-        return passwordEncrypted;
+        return passwordEncrypted.getBytes(StandardCharsets.UTF_8);
     }
 
     public Date getCreated() {
@@ -39,6 +44,22 @@ public class Account {
     }
 
     public void setPasswordEncrypted(byte[] passwordEncrypted) {
-        this.passwordEncrypted = passwordEncrypted;
+        this.passwordEncrypted = new String(passwordEncrypted, StandardCharsets.UTF_8);
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
     }
 }
