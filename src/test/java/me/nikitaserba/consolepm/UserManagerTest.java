ad3fbcdeee1fffe9a777b8371f26ca0f74ffdbe0
Nikita Serba <nikitaserba@icloud.com>
2025-04-05 22:54:03 +0300

    Add tests for user management

---
+++ src/test/java/me/nikitaserba/consolepm/UserManagerTest.java
@@ -0,0 +1,31 @@
+package me.nikitaserba.consolepm;
+
+import me.nikitaserba.consolepm.utils.MemoryDataManager;
+import me.nikitaserba.consolepm.utils.exceptions.NoSuchUserException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserManagerTest {
+
+    UserManager userManager;
+
+    @BeforeEach
+    void setUp() throws IOException {
+        userManager = UserManager.getInstance(new MemoryDataManager());
+    }
+
+    @Test
+    void createNewUser() throws IOException, NoSuchUserException {
+        userManager.newUser("testuser", "testpassword");
+
+        assertTrue(userManager.checkPassword("testuser", "testpassword"),
+                "Password was correct but authentication failed");
+        assertFalse(userManager.checkPassword("testuser", "test"),
+                "True was given for false password");
+        assertThrows(NoSuchUserException.class, () -> userManager.checkPassword("testuser2", ""));
+    }
+}
